e257cedf49c2d777a4274dc61398ac1ac69018fc
Mystriodis <83100307+Mystriodis@users.noreply.github.com>
2024-02-13 20:18:23 -0500

    deleting head thing

--- Assets/Scripts/pillManager.cs
+++ Assets/Scripts/pillManager.cs
@@ -2,18 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<<< HEAD:Assets/Scripts/NewScripts/sideManagerV2.cs
-public class sideManagerV2 : MonoBehaviour
-========
 public class pillManager : MonoBehaviour
->>>>>>>> development-david:Assets/Scripts/pillManager.cs
 {
     //acts as the game manager for each player
     //does so by managing and storing the "state" variable 
     //possible states: selection, move, syringe
 
     //these scripts mainly just pass through input to other scripts
-    [SerializeField] private pillSelectionV2 selectionScript;   //just changed to reference new script
+    [SerializeField] private pillSelection selectionScript;   //just changed to reference new script
     [SerializeField] private pillMove moveScript;
     [SerializeField] private Transform gridCenter;
     [SerializeField] private Vector2 gridSize; //HALF SIZE OF GRID
@@ -29,11 +25,7 @@ public class pillManager : MonoBehaviour
 
     private void passVariables()
     {
-<<<<<<<< HEAD:Assets/Scripts/NewScripts/sideManagerV2.cs
-
-========
         moveScript.gridSize = gridSize;
->>>>>>>> development-david:Assets/Scripts/pillManager.cs
     }
 
     public void switchState(string newState)
