553a072276cf81e1a359c3049e569e8a81e0e213
davidsong9719 <davidsong9719@gmail.com>
2024-02-20 15:56:01 -0500

    Syringe connection

--- Assets/Scripts/connectionCheck.cs
+++ Assets/Scripts/connectionCheck.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,13 +8,12 @@ public class connectionCheck : MonoBehaviour
 {
     public string currentObject = "";
     public bool connected = false;
-    public GameObject connectedObject;
+    public GameObject connectedObject, connectedObject1;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        print(currentObject);
     }
 
     // Update is called once per frame
@@ -33,8 +33,12 @@ public class connectionCheck : MonoBehaviour
             case "phone1": //phone bottom
                 checkPhonePosition("phone0");
             break;
-            
-            
+
+            case "syringe0":
+            case "syringe1":
+            case "syringe2":
+                checkSyringePosition();
+            break;
         }
     }
 
@@ -51,6 +55,62 @@ public class connectionCheck : MonoBehaviour
         {
             startCall(neighborSegment);
         }
+    }
+
+    private void checkSyringePosition()
+    {
+        Vector2 castPosition0 = Vector2.zero; 
+        Vector2 castPosition1 = Vector2.zero;
+        string target0 = "";
+        string target1 = "";
+        Vector2 syringeDirection = (transform.position - transform.parent.position).normalized;
+        if (currentObject == "syringe2") syringeDirection = -syringeDirection;
+
+        switch (currentObject)
+        {
+            case "syringe0":
+                castPosition0 = (Vector2)transform.position + syringeDirection;
+                castPosition1 = (Vector2)transform.position + (syringeDirection * 2);
+
+                target0 = "syringe1";
+                target1 = "syringe2";
+            break;
+
+            case "syringe1":
+                castPosition0 = (Vector2)transform.position - syringeDirection;
+                castPosition1 = (Vector2)transform.position + syringeDirection;
+
+                target0 = "syringe0";
+                target1 = "syringe2";
+            break;
+
+            case "syringe2":
+                castPosition0 = (Vector2)transform.position - syringeDirection;
+                castPosition1 = (Vector2)transform.position - syringeDirection * 2;
+
+                target0 = "syringe1";
+                target1 = "syringe0";
+            break;
+
+            default:
+                return;
+        }
+
+        RaycastHit2D hit0 = Physics2D.Raycast(castPosition0, Vector2.zero);
+        RaycastHit2D hit1 = Physics2D.Raycast(castPosition1, Vector2.zero);
+
+        if (!hit0 || !hit1) return;
+        if (hit0.collider.gameObject.GetComponent<connectionCheck>() == null) return;
+        if (hit1.collider.gameObject.GetComponent<connectionCheck>() == null) return;
+
+        connectionCheck neighborSegment0 = hit0.collider.gameObject.GetComponent<connectionCheck>();
+        connectionCheck neighborSegment1 = hit1.collider.gameObject.GetComponent<connectionCheck>();
+
+        if (neighborSegment0.currentObject == target0 && neighborSegment1.currentObject == target1)
+        {
+            print("WORKS");
+        }
+
     }
 
     private void startCall(connectionCheck neighborSegment)
